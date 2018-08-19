13aa2bd1e1e901cd8fa55a88331390bd956da9a7
Kavilan Nair <kavilann@gmail.com>
2018-08-19 21:52:37 +0200

    Added post to support firebase wack urls

--- app.py
+++ app.py
@@ -45,11 +45,66 @@ def exists(sound_url):
     else:
         return "true"
 
+
+
+
+@app.route('/classifyPost', methods=['POST'])
+def classifyPost():
+    req_data = request.get_json()
+    sound_url = req_data['url']
+    sound_name = sound_url.split('%')[-1]
+    sound_name = sound_name.split('?')[0]
+    print(sound_url)
+    print(sound_name)
+    file_name = sound_name
+    if not os.path.exists(file_name):
+        with urllib.request.urlopen(sound_url) as response:
+            with open(file_name, 'wb') as out_file:
+                data = response.read() 
+                out_file.write(data)
+  
+
+    y, fs = librosa.load(file_name,sr=None,mono=True)
+    message = "MFCC was generated!"
+    mfccs = librosa.feature.mfcc(y, sr=fs, n_fft=1024, hop_length=512, n_mfcc=13, fmin=0,fmax=8000)
+    mfccs = sklearn.preprocessing.scale(mfccs, axis=1)  
+    librosa.display.specshow(mfccs, sr=fs*2, cmap='coolwarm')
+    imageName = sound_name[:-3] + 'png'
+    save_image(imageName)
+    image = cv2.imread(imageName)
+    output = image.copy()
+
+    image = cv2.resize(image, (200, 100))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    
+    print("[INFO] loading network...")
+    model = load_model(modelName)
+    lb = pickle.loads(open(labelName, "rb").read())
+
+    print("[INFO] classifying image...")
+    proba = model.predict(image)[0]
+    idx = np.argmax(proba)
+    label = lb.classes_[idx].decode('ASCII')
+    label = "{}: {:.2f}%".format(label, proba[idx] * 100)
+    K.clear_session()
+
+    return label
+
+
+
+    return "FUCK YOU"
+
+
+
 @app.route('/classify/<path:sound_url>', methods=['GET'])
 def classify(sound_url):
 
     sound_name = sound_url.split('/')[-1]
     file_name = 'Download/' + sound_name
+    print("name: " + sound_name)
+    print("url: " + sound_url)
     if not os.path.exists(file_name):
         with urllib.request.urlopen(sound_url) as response:
             with open(file_name, 'wb') as out_file:
